c67f51413153a556a4dace36008200aada349122
uttnaoki <uttnaoki@gmail.com>
2023-05-09 01:11:43 +0900

    DBのデータを整形して表示

--- web/src/components/Post/Posts/Posts.tsx
+++ web/src/components/Post/Posts/Posts.tsx
@@ -1,11 +1,11 @@
+import type { DeletePostMutationVariables, FindPosts } from 'types/graphql'
+
 import { Link, routes } from '@redwoodjs/router'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 
 import { QUERY } from 'src/components/Post/PostsCell'
 import { timeTag, truncate } from 'src/lib/formatters'
-
-import type { DeletePostMutationVariables, FindPosts } from 'types/graphql'
 
 const DELETE_POST_MUTATION = gql`
   mutation DeletePostMutation($id: Int!) {
